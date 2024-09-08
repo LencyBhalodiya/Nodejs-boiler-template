611e7873ae6406badcf83d33497f58249f3a85eb
Lencybhalodiya <worklency@gmail.com>
2024-09-08 15:57:59 +0530

    prettier setup

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -10,7 +10,7 @@ import {
 
 const router = express.Router();
 
-router.post('/register',      register);
+router.post('/register', register);
 router.post('/login', login);
 router.post('/refresh-token', refreshTokens);
 router.post('/logout', logout);
